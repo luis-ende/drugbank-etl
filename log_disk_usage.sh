9f015c6c156dc8c3574958f9bc06e93afbd80f6a
Jose Luis Salazar <jsalazar@supernus.com>
2022-03-24 13:08:40 -0600

    Add logging of disk usage for the Drugbank data directories

--- log_disk_usage.sh
+++ log_disk_usage.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 DISK_USAGE_REPORT=$(df -hT)
+DRUGBANK_USAGE_REPORT=$(du -h --max-depth=0 /opt/sas_drugbank/data/drugbank_json /opt/sas_drugbank/data/drugbank_csv/)
 REPORT_DATE=$(date)
 
 echo "--- sil.supnapps.com Disk Usage Report $REPORT_DATE ---" >> logs/sil_disk_usage.log
 echo "$DISK_USAGE_REPORT" >> logs/sil_disk_usage.log
+echo "--- DrugBank directories disk usage report ---" >> logs/sil_disk_usage.log
+echo "$DRUGBANK_USAGE_REPORT" >> logs/sil_disk_usage.log
